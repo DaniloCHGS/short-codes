e62028f87f7e7397d1d29bf705f0306bb27d55c6
DaniloCHGSDev <danilobm500@gmail.com>
2023-01-19 11:31:09 -0300

    Code to make sibling elements with dynamic contents the same size

---
+++ js/autoHeight/autoHeight.js
@@ -0,0 +1,9 @@
+const autoHeight = (attr) => {
+    const elements = [...document.querySelectorAll(`[data-auto-height=${attr}]`)]
+    let elementsHeight = elements.map(element => element.offsetHeight)
+    let height = Math.max.apply(null, elementsHeight)
+
+    elements.forEach(element => element.style.height = `${height}px`)
+}
+
+export { autoHeight }
